57b22a471823b7d7ebb9c31cce831d088f40078a
Kevin Kabatra <kevin@lsretail.com>
2020-05-01 16:48:06 +0000

    Added support for two digit operations and CECIL
    1. CECIL is the random number generator from Countdown.
    2. The two digit operations can add, subtract, multiply, and divide.
    3. Added unit tests for two digit operations.

---
+++ Kabatra.Countdown.Numbers/Kabatra.Countdown.Numbers.SolutionGenerator/TwoDigitOperations.cs
@@ -0,0 +1,25 @@
+﻿namespace Kabatra.Countdown.Numbers.SolutionGenerator
+{
+    public class TwoDigitOperations
+    {
+        public static int Add(int numberOne, int numberTwo)
+        {
+            return numberOne + numberTwo;
+        }
+
+        public static int Subtract(int numberOne, int numberTwo)
+        {
+            return numberOne - numberTwo;
+        }
+
+        public static int Multiply(int numberOne, int numberTwo)
+        {
+            return numberOne * numberTwo;
+        }
+
+        public static int Divide(int numberOne, int numberTwo)
+        {
+            return numberOne / numberTwo;
+        }
+    }
+}
